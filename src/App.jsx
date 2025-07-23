bceab157c3741ce93ad76d1702bbb83c42f3f738
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-23 20:04:31 +0200

    App enrouted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <h1>Uone</h1>
     </>
   );
