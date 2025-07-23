d6d5a57d3c90f188f241ae36be04a1b4ff118ff4
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-23 20:08:51 +0200

    App enrouted

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
